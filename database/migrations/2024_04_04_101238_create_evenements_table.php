d0caeb6e56f35a3b1bdf0c9c184cc0a86a8b7c3c
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2024-04-04 12:48:50 +0200

    Création classe modèle Evenement #19

--- database/migrations/2024_04_04_101238_create_evenements_table.php
+++ database/migrations/2024_04_04_101238_create_evenements_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->enum('type', ['THEATRE', 'CINEMA', 'CONCERT', 'FESTIVAL', 'COMPETITION']);
             $table->string('description');
             $table->datetime('date_event');
-            $table->timestamps();
         });
     }
 
