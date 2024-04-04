0ad1acf93be1cf3257a7f04e9ba86f1d6b9764e7
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2024-04-04 12:49:10 +0200

    Création fichier migration reservation close #19

---
+++ database/migrations/2024_04_04_102352_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Evenement;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('date_res');
+            $table->integer('nb_billets')->nullable();
+            $table->double('montant')->nullable();
+            $table->enum('statut', ['EN ATTENTE', 'PAYE', 'ANNULE', 'BILLET_EDITE']);
+            $table->foreignIdFor(Evenement::class)->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
