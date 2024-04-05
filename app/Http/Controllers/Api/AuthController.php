7262c40d6e4d181ec78d9a96924aa3fa545cfe52
Roméo Jacquemin <romeo_jacquemin@ens.univ-artois.fr>
2024-04-05 09:55:51 +0200

    Ajout des fonctions d'enregistrement, de connexion et de déconnection close #2 close #3 close #4

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+//use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use OpenApi\Attributes as OA;
+
+
+class AuthController extends Controller {
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6',
+        ], [
+            'required' => 'Le champ :attribute est obligatoire',
+            'email' => 'L\'adresse mail n\'est pas correcte',
+            'min' => 'Le champ :attribute doit contenir au minimum :min caractères.',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'BONJOUR, JE SUIS LE ROI DRAGON',
+            'user' => new UserResource($user),
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+
+    public function register(UserRequest $request) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $roleVisiteur = Role::where('nom', Role::VISITEUR)->first();
+        $user->roles()->attach([$roleVisiteur->id]);
+        $token = auth()->tokenById($user->id);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Bienvenue dans notre communauté',
+            'user' => new UserResource($user),
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+
+    public function logout() {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+
+    public function refresh() {
+        return response()->json([
+            'status' => 'success',
+            'user' => new UserResource(Auth::user()),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+
+    public function me() {
+        return response()->json([
+            'status' => 'success',
+            'user' => new UserResource(Auth::user()),
+        ]);
+    }
+}
