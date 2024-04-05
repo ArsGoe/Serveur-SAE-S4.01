09088b1603f53d44d125741443396dcdc5e9808d
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2024-04-05 09:31:48 +0200

    Modif fichier migration artiste #26

--- database/migrations/2024_04_04_105523_create_artistes_table.php
+++ database/migrations/2024_04_04_105523_create_artistes_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id();
             $table->string('nom');
             $table->enum('genre', Genre::getValues());
-            $table->timestamps();
         });
     }
 
