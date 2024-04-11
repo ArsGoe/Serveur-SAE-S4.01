3cc4cbc76a272d44a48ca79ca9e93afb6264bd75
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2024-04-11 09:06:38 +0200

    Ajout route paiement #51

--- routes/api.php
+++ routes/api.php
@@ -109,6 +109,9 @@ Route::prefix('reservations')->group(function () {
     Route::delete("/{id}", [ReservationController::class, 'destroy'])->where('id', '[0-9]+')
         ->middleware(['auth', 'role:GESTIONNAIRE'])
         ->name('reservations.destroy');
+    Route::post('/{id}/paiement', [ReservationController::class, 'paiement'])
+        ->middleware(['auth', 'role:ACTIF'])
+        ->name('reservations.paiement');
 });
 
 Route::get('/lieux', [EvenementController::class, 'lieux'])
